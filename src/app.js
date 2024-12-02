0d8d871d7aeb84ee6f824c7679946e6d4db9f114
Ilyas <hopi@mail.ru>
2024-12-02 19:23:15 +0400

    lesson18/04 2dec2024

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
-"use strict";
+import { MainView } from "../static/views/main/main";
 
-function a() {}
+class App {
+  routes = [{ path: "", view: MainView }];
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this));
+    this.route();
+  }
+
+  route() {
+    if (this.currentView) {
+      this.currentView.destroy();
+    }
+    const view = this.routes.find((r) => r.path == location.hash).view;
+    this.currentView = new view();
+    this.currentView.render();
+  }
+}
+
+new App();
