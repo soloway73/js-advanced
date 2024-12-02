0fc21c5ccf2c05fcfdea79e46992a462cd771cad
Ilyas <hopi@mail.ru>
2024-12-02 15:12:12 +0400

    start2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+//import js from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  //js.configs.recommended,
   pluginJs.configs.recommended,
+  {
+    languageOptions: { globals: globals.browser },
+    rules: { "no-unused-vars": "warn", "no-undef": "warn" },
+  },
 ];
